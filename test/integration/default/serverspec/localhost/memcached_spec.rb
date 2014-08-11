250ccbb59aba3346447d80a96bc316ddbcf9144d
Erik Ljungstrom <erik5673@eljungst-mb.rackspace.corp>
2014-08-11 17:05:07 +0100

    Functioning multi-platform serverspec tests. Updated Strainer to use kitchen for testing as well

--- test/integration/default/serverspec/localhost/memcached_spec.rb
+++ test/integration/default/serverspec/localhost/memcached_spec.rb
@@ -1,7 +1,27 @@
 require 'spec_helper'
 
+describe package('memcached') do
+  it { should be_installed }
+end
+
+if os[:family] == 'RedHat' 
+  describe file('/etc/sysconfig/memcached') do
+    it { should be_file }
+    its(:content) { should match /PORT="11211"/ }
+  end
+else
+  describe file('/etc/memcached.conf') do
+    it { should be_file }
+    its(:content) { should match /-p\ 11211/ }
+  end
+end
+
 describe service('memcached') do  
   it { should be_enabled   }
   it { should be_running   }
 end  
+
+describe port(11211) do
+  it {should be_listening }
+end
 
