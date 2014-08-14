459c0d399d082e4d833af95a00741c58910eaf54
Erik Ljungstrom <erik.ljungstrom@rackspace[-dot-]co.uk>
2014-08-14 10:18:52 +0000

    Added custom config hash for debian platforms

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,10 @@
 default['memcached']['memory_share'] = 0.75
 default['memcached']['port'] = 112_12
+
+# Populate with any custom config in k/v format
+default['memcached']['debian']['config']['memcached.conf'] = {
+    '#-v' => '',
+    '-c' => '500',
+    '-M' => '',
+
+}
